f676c58646ac61f1115bfeaab62ddd148abd3d58
Allan Tribe <atribe13@gmail.com>
2014-02-17 01:29:41 -0700

    Initial Commit with Code Lessons learned: 1. logger I guess I kinda figured it out 2. SpringMVC is working perfectly        Differences, index.jsp is at the root of webapp        Logger is present        Not using a dispatcher-servlet.xml, using the default one I guess.

---
+++ src/main/java/com/atomrockets/marketanalyzer/controller/AccountController.java
@@ -0,0 +1,34 @@
+package com.atomrockets.marketanalyzer.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/account")
+public class AccountController {
+	/* Get actual class name to be printed on */
+	static Logger log = Logger.getLogger(AccountController.class.getName());
+
+    @RequestMapping(value = "/account", method = RequestMethod.GET)
+    public ModelAndView findAllAccounts() throws Exception {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("account");
+        mav.addObject("someText", "Listing all accounts!");
+        
+        log.info("----------------findAllAccounts Test----------------");
+        return mav;
+    }
+    
+    @RequestMapping(value="/{accountId}", method = RequestMethod.GET)
+    public ModelAndView findAccount(@PathVariable int accountId, Model model) {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("account");
+        mav.addObject("someText", String.format("Showing account %d", accountId));
+        return mav;
+    }   
+}
