9e8eac1da07cd81478c954c8da964c296eecd6d8
Allan Tribe <atribe13@gmail.com>
2014-02-17 22:34:19 -0700

    Integrated the property thing from that one tutorial. Easier to change based on the environment.

--- src/main/java/com/atomrockets/marketanalyzer/something/testMain.java
+++
@@ -1,9 +0,0 @@
-package com.atomrockets.marketanalyzer.something;
-
-public class testMain {
-
-	public static void pizza() {
-		System.out.println("testing, testing, 1,2,3. I mean Hello World");
-	}
-
-}
--- src/main/java/com/atomrockets/marketanalyzer/spring/init/AppInitializer.java
+++ src/main/java/com/atomrockets/marketanalyzer/spring/init/AppInitializer.java
@@ -1,5 +1,7 @@
 package com.atomrockets.marketanalyzer.spring.init;
 
+import java.util.Properties;
+
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
@@ -11,13 +13,19 @@ import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-import com.atomrockets.marketanalyzer.something.testMain;
 import com.atomrockets.marketanalyzer.spring.controller.AccountController;
+import com.atomrockets.marketanalyzer.spring.init.PropertiesLoader;
 
 public class AppInitializer implements WebApplicationInitializer {
 	/* Get actual class name to be printed on */
 	static Logger log = Logger.getLogger(AccountController.class.getName());
 	private static final String CONFIG_LOCATION = "com.atomrockets.marketanalyzer.spring.config";
+	private static final String MAPPING_URL = "/";
+    
+    private static final String SPRING_PROPERTIES_FILE_NAME = "spring.properties";
+    private static final String ACTIVE_PROFILE_PROPERTY_NAME = "spring.profiles.active";
+    private static final String DEFAULT_PROFILE = "dev";
+    private static final PropertiesLoader propertiesLoader = new PropertiesLoader();
 	
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
@@ -27,14 +35,18 @@ public class AppInitializer implements WebApplicationInitializer {
         servletContext.addListener(new ContextLoaderListener(context));
         ServletRegistration.Dynamic dispatcher = servletContext.addServlet("DispatcherServlet", new DispatcherServlet(context));
         dispatcher.setLoadOnStartup(1);
-        dispatcher.addMapping("/");
+        dispatcher.addMapping(MAPPING_URL);
         
-        testMain.pizza();
+        log.info("Startup index analysis begining");
+        
     }
 
     private AnnotationConfigWebApplicationContext getContext() {
         AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
         context.setConfigLocation(CONFIG_LOCATION);
+        
+        Properties prop = propertiesLoader.load(SPRING_PROPERTIES_FILE_NAME);
+        context.getEnvironment().setActiveProfiles(prop.getProperty(ACTIVE_PROFILE_PROPERTY_NAME, DEFAULT_PROFILE));
         return context;
     }
 
