59af645f3d14ae11cf7386c381f70386f8a39e00
Allan Tribe <atribe13@gmail.com>
2014-12-09 23:40:39 -0700

    Fixed the ability to run on localhost and on openshift. Also fixed the css so it would change based on the width of the window.

--- src/main/webapp/WEB-INF/resources/js/stockchart.js
+++ src/main/webapp/WEB-INF/resources/js/stockchart.js
@@ -3,8 +3,17 @@
  */
 
 $(function() {
-    console.log( "ready!" );
-    $.getJSON("/json/highstockohlcv/test",
+    console.log( "stockchart.js ready!" );
+    //alert(document.URL);
+    
+    var url = "/json/highstockohlcv/test";
+    var addtourl = "/marketanalyzer";
+    
+    if(document.URL == "http://localhost:8080/marketanalyzer/") {
+    	url = addtourl.concat(url);
+    }
+    
+    $.getJSON(url,
     	function(json) {
     	
     	var chartJson = eval(json);
