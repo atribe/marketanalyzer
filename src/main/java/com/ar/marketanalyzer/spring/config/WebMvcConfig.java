4c8e587405f50f642b03968b7f4c7f7b3b58f14d
Allan <atribe13@gmail.com>
2015-01-01 23:30:35 -0700

    This is from the tutorial mentioned a couple commits ago. I'll follow it through for one more day and then this might get rolled back.

--- src/main/java/com/ar/marketanalyzer/spring/config/WebMvcConfig.java
+++ src/main/java/com/ar/marketanalyzer/spring/config/WebMvcConfig.java
@@ -1,22 +1,31 @@
 package com.ar.marketanalyzer.spring.config;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import javax.xml.transform.Source;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.ResourceHttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.support.AllEncompassingFormHttpMessageConverter;
 import org.springframework.http.converter.xml.SourceHttpMessageConverter;
+import org.springframework.web.accept.ContentNegotiationManager;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 @EnableWebMvc
 @Configuration
@@ -28,12 +37,25 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 	private JacksonConfig jacksonConfig;
 	
 	@Bean
-	 public InternalResourceViewResolver configureInternalResourceViewResolver() {
-		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-		resolver.setPrefix("/WEB-INF/views/");
-		resolver.setSuffix(".jsp");
+	public ViewResolver contentNegotiatingViewResolver(
+			ContentNegotiationManager manager) {
 		
-		return resolver;
+		List<ViewResolver> resolvers = new ArrayList<ViewResolver>();
+		
+		InternalResourceViewResolver r1 = new InternalResourceViewResolver();
+		r1.setPrefix("/WEB-INF/views/");
+		r1.setSuffix(".jsp");
+		r1.setViewClass(JstlView.class);
+		resolvers.add(r1);
+		
+		JsonViewResolver r2 = new JsonViewResolver();
+		resolvers.add(r2);
+		
+		ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
+		resolver.setViewResolvers(resolvers);
+		resolver.setContentNegotiationManager(manager);
+	    return resolver;
+	    
 	}
 	
 	/*
@@ -58,4 +80,31 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
         converters.add(new AllEncompassingFormHttpMessageConverter());
         converters.add(jacksonConfig.jackson2Converter());
     }
+	
+	/**
+	* View resolver for returning JSON in a view-based system. Always returns a
+	* {@link MappingJacksonJsonView}.
+	*/
+	public class JsonViewResolver implements ViewResolver {
+		public View resolveViewName(String viewName, Locale locale)
+				throws Exception {
+				MappingJacksonJsonView view = new MappingJacksonJsonView();
+				view.setPrettyPrint(true);
+				return view;
+		}
+	}
+	
+	/*
+	 * From http://fruzenshtein.com/spring-mvc-ajax-jquery/
+	 * For use with REST controller
+	 */
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		configurer.favorPathExtension(true)
+			.useJaf(false)
+			.ignoreAcceptHeader(true)
+			.mediaType("html", MediaType.TEXT_HTML)
+			.mediaType("json", MediaType.APPLICATION_JSON)
+			.defaultContentType(MediaType.TEXT_HTML);
+	}
 }
